f9ecbd3b6c72e85befdb6781d4e042b786119de8
girin-dev <kw8799@gmail.com>
2017-12-14 20:19:20 +0900

    pet-state component is moved from wooltari, SunnySunwha

---
+++ wooltari/src/app/dev/pet-state/pet-state.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-pet-state',
+  templateUrl: './pet-state.component.html',
+  styleUrls: ['./pet-state.component.scss']
+})
+export class PetStateComponent implements OnInit {
+  stateForm: FormGroup;
+  constructor(
+    @Inject(FormBuilder) private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.stateForm = this.fb.group({
+      currentWeight: ['', Validators.required],
+      goalWeight: ['', Validators.required],
+      neckSize: ['', Validators.required],
+      chestSize: ['', Validators.required]
+    });
+  }
+}
